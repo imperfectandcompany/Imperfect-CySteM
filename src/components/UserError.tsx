896aa5b38a7699b91143ecadcd803831e335b979
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2024-07-31 21:46:02 -0400

    Enhance error handling and form progression in UI components
    This commit introduces several improvements to error displays and form handling logic across the `AdminError` and `SupportForm` components:
    
    - `AdminError`: Extended to accept an optional `subMessage` prop, allowing for more detailed error messages. This change involves updating the component's prop types and rendering logic to display this additional message if provided.
    
    - `SupportForm`: Refactored the issue change handlers and progress calculation to improve the user experience:
      - Modified `handleIssueChange` and `handleSubIssueChange` to reset relevant states more cleanly, ensuring that changes in the issue or sub-issue selections correctly reset the form's progression and input fields.
      - Updated the progress calculation in the main useEffect hook. The logic now accounts for all form elements (email, main issue, sub-issue, and individual inputs) more accurately. It ensures that progress towards form completion is calculated based on the actual number of filled fields relative to the total required fields, preventing premature progression to 100% completion.
      - Introduced error handling with `UserError` for more context-specific feedback when the form encounters issues, replacing a basic text display with a more structured error component that can also display a sub-message.
    
    These changes are aimed at making the administrative and support interfaces more intuitive and responsive to user inputs, enhancing overall usability and error feedback.

--- src/components/UserError.tsx
+++ src/components/UserError.tsx
@@ -1,7 +1,7 @@
 import { FunctionalComponent } from "preact";
 import { Link } from "preact-router/match";
 
-export const AdminError: FunctionalComponent<{ message: string, subMessage?: string }> = ({ message, subMessage }) => {
+export const UserError: FunctionalComponent<{ message: string, subMessage?: string }> = ({ message, subMessage }) => {
   return (
     <div className="container mx-auto relative px-8 py-16 max-w-7xl md:px-12 lg:px-18 lg:py-22">
       <h1 className="text-3xl font-normal tracking-tighter text-black sm:text-4xl lg:text-5xl">
@@ -10,8 +10,8 @@ export const AdminError: FunctionalComponent<{ message: string, subMessage?: str
      {subMessage && <p className="mt-4">
         {subMessage}
       </p>}
-      <Link href="/admin/dashboard" className="text-indigo-500 hover:underline mt-8 inline-block">
-        Return to Admin Dashboard
+      <Link href="/" className="text-indigo-500 hover:underline mt-8 inline-block">
+        Go to home
       </Link>
     </div>
   );
